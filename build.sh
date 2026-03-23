2e7c96a013b05a5fc7ff2a31a11e59c2e8c30230
Felipe da Cruz Marques <felipedacruzmarques23@gmail.com>
2026-03-23 13:37:59 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@email.com', 'senha123')
+    print('Superusuário criado!')
+else:
+    print('Superusuário já existe.')
+"
