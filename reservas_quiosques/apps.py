066f5f214a1787e343984ce5dee2146c209e3eba
Felipe da Cruz Marques <felipedacruzmarques23@gmail.com>
2026-03-23 16:18:30 -0300

    Add default_auto_field and verbose_name to app config

--- reservas_quiosques/apps.py
+++ reservas_quiosques/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class ReservasQuiosquesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'reservas_quiosques'
+    verbose_name = 'Reservas e Quiosques'
