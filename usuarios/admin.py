989fe0d4e3c0919dea3eb484da4e57d9b2eec014
Felipe da Cruz Marques <204526472+FelipeMarques-bot@users.noreply.github.com>
2026-06-02 00:24:38 +0000

    Modernizar dashboard admin e adicionar botões de bloqueio na lista de usuários

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin, messages
 from django.contrib.admin.sites import NotRegistered
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.urls import path, reverse
+from django.utils.html import format_html
 
 
 @admin.action(description='Bloquear usuários selecionados')
@@ -25,10 +28,71 @@ def desbloquear_usuarios(modeladmin, request, queryset):
 
 
 class UserAdmin(BaseUserAdmin):
-	list_display = ('username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active')
+	change_list_template = 'admin/auth/user/change_list.html'
+	list_display = (
+		'username',
+		'first_name',
+		'last_name',
+		'email',
+		'is_staff',
+		'status_acesso',
+		'acoes_acesso',
+	)
 	list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-	list_editable = ('is_active',)
 	actions = (bloquear_usuarios, desbloquear_usuarios)
+
+	def get_urls(self):
+		urls = super().get_urls()
+		custom_urls = [
+			path(
+				'<int:user_id>/bloquear/',
+				self.admin_site.admin_view(self.bloquear_usuario_view),
+				name='auth_user_bloquear',
+			),
+			path(
+				'<int:user_id>/desbloquear/',
+				self.admin_site.admin_view(self.desbloquear_usuario_view),
+				name='auth_user_desbloquear',
+			),
+		]
+		return custom_urls + urls
+
+	@admin.display(description='Status de Acesso')
+	def status_acesso(self, obj):
+		if obj.is_active:
+			return format_html('<span class="bv-user-status is-active">Ativo</span>')
+		return format_html('<span class="bv-user-status is-blocked">Bloqueado</span>')
+
+	@admin.display(description='Ações')
+	def acoes_acesso(self, obj):
+		if obj.is_active:
+			url = reverse('admin:auth_user_bloquear', args=[obj.pk])
+			return format_html(
+				'<a class="bv-user-inline-action is-block" href="{}">Bloquear</a>',
+				url,
+			)
+
+		url = reverse('admin:auth_user_desbloquear', args=[obj.pk])
+		return format_html(
+			'<a class="bv-user-inline-action is-unblock" href="{}">Desbloquear</a>',
+			url,
+		)
+
+	def bloquear_usuario_view(self, request, user_id):
+		user = User.objects.filter(pk=user_id).first()
+		if user:
+			user.is_active = False
+			user.save(update_fields=['is_active'])
+			self.message_user(request, f'Usuário {user.username} bloqueado com sucesso.', level=messages.SUCCESS)
+		return HttpResponseRedirect(reverse('admin:auth_user_changelist'))
+
+	def desbloquear_usuario_view(self, request, user_id):
+		user = User.objects.filter(pk=user_id).first()
+		if user:
+			user.is_active = True
+			user.save(update_fields=['is_active'])
+			self.message_user(request, f'Usuário {user.username} desbloqueado com sucesso.', level=messages.SUCCESS)
+		return HttpResponseRedirect(reverse('admin:auth_user_changelist'))
 
 
 try:
