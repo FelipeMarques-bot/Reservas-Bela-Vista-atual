fd30d7ac0755968851f128a414939b9f909c78fb
Felipe Marques <felipe@seu-email.com>
2026-06-03 14:33:56 -0300

    remove makemigrations do build do deploy

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py makemigrations
 python manage.py shell -c "
 from django.contrib.auth.models import User
 if not User.objects.filter(username='admin').exists():
